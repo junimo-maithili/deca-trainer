01b4f9b5d591cf3fdb2a86e98a309a8793067614
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-31 01:17:27 -0400

    Removed .env variables

--- backend/utils/client.py
+++ backend/utils/client.py
@@ -1,11 +1,6 @@
-
-from dotenv import load_dotenv
 import os
-from pathlib import Path
 import google.generativeai as genai
 
-# LLM setup
-env_path = Path(__file__).resolve().parent.parent / "frontend" / ".env"
-load_dotenv(dotenv_path=env_path)
+# No need to load .env manually
 key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=key)
