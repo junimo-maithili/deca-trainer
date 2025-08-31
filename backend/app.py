c671e429c952ce884c47eb897462326b38b428fc
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-31 10:57:11 -0400

    Added debug stuff

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 import os
-
-import os
-from flask import Flask
+from routes.ocr_routes import ocr_bp
+from routes.gemini_routes import gemini_bp
+from routes.opencv_routes import opencv_bp
 
 app = Flask(__name__)
+
+
+app.register_blueprint(ocr_bp)
+app.register_blueprint(gemini_bp)
+app.register_blueprint(opencv_bp)
+
+
 
 @app.route("/")
 def home():
