7052e1d43893b1bde0e22e82dec72c72d84fe1b9
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-31 01:47:13 -0400

    SImplified app.py for debugging

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,19 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Railway!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
+
+'''
 from flask import Flask
 from routes.ocr_routes import ocr_bp
 from routes.gemini_routes import gemini_bp
@@ -35,3 +49,5 @@ def test():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+'''
