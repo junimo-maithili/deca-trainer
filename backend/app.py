954a7bec584816183251059532b3c8e3611f3452
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-30 22:46:42 -0400

    Bug fixes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-'''
+
 
 from flask import Flask
 from routes.ocr_routes import ocr_bp
@@ -7,32 +7,15 @@ from routes.opencv_routes import opencv_bp
 from flask_cors import CORS
 import os
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, origins=["https://presentation-trainer.vercel.app/"])
-
-    app.register_blueprint(ocr_bp)
-    app.register_blueprint(gemini_bp)
-    app.register_blueprint(opencv_bp)
-
-    return app
-
-app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-'''
-from flask import Flask
-
 app = Flask(__name__)
+CORS(app, origins=["https://presentation-trainer.vercel.app/"])
 
-@app.route('/')
-def test():
-    return "wokring"
+app.register_blueprint(ocr_bp)
+app.register_blueprint(gemini_bp)
+app.register_blueprint(opencv_bp)
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
