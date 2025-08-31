bed31cf3e16687743aa0333c9e39b65545b5a650
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-31 10:24:17 -0400

    Recommit for deployment

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 def home():
     return "Hello from Railway!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
 '''
 
