9215e30968c7b893719967627c462cb9ebeda825
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-30 22:15:35 -0400

    Changed flaskenv for deployment

--- backend/.flaskenv
+++ backend/.flaskenv
@@ -1,2 +1,5 @@
-FLASK_APP=app:create_app
-FLASK_ENV=development
+export FLASK_APP=create_app
+export FLASK_ENV=development
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=5000
+flask run
--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,6 @@ from routes.opencv_routes import opencv_bp
 from flask_cors import CORS
 import os
 
-print("running")
-
 def create_app():
     app = Flask(__name__)
     CORS(app, origins=["https://presentation-trainer.vercel.app/"])
@@ -18,9 +16,7 @@ def create_app():
     return app
 
 app = create_app()
-print("created")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    print("run")
