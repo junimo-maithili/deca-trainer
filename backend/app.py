085845e1c559a1e79010f9db2f0b467628c6d432
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-31 10:29:04 -0400

    Changed app.py

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 def home():
     return "Hello from Railway!"
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 '''
 
